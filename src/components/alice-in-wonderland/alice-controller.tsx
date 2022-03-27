9d4eb507f70cf3d8164371ae37454c053104a8b8
yoonminsang <apdlyooapdl@naver.com>
2022-03-27 12:19:53 +0900

    reafactor: 앨리스 컨트롤러 분리

---
+++ src/components/alice-in-wonderland/alice-controller.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useState } from 'react';
+import Button from '../common/button';
+
+const ONE_BEVERAGE = 20;
+const ONE_CAKE = 50;
+const ONE_MUSHROOM = 10;
+
+interface Props {
+  onChangeHeight: (height: number) => void;
+  onGoOutside: () => void;
+}
+
+const AliceController: React.FC<Props> = ({ onChangeHeight, onGoOutside }) => {
+  const [, setBeverage] = useState<number>(200); // ml
+  const [, setCake] = useState<number>(1000); // g
+  const [, setMushromm] = useState<number>(100); // g
+
+  const onDrkinkBeverage = useCallback(() => {
+    setBeverage((beverage) => {
+      if (beverage < ONE_BEVERAGE) {
+        alert('음료수가 없습니다');
+        return beverage;
+      }
+      return beverage - ONE_BEVERAGE;
+    });
+    onChangeHeight(-100);
+  }, [onChangeHeight]);
+
+  const onEatCake = useCallback(() => {
+    setCake((cake) => {
+      if (cake < ONE_CAKE) {
+        alert('케익이 없습니다');
+        return cake;
+      }
+      return cake - ONE_CAKE;
+    });
+    onChangeHeight(+80);
+  }, [onChangeHeight]);
+
+  // 부채질
+  const onFan = useCallback(() => {
+    onChangeHeight(+70);
+  }, [onChangeHeight]);
+
+  const onEatMushroom = useCallback(() => {
+    setMushromm((mushroom) => {
+      if (mushroom < ONE_MUSHROOM) {
+        alert('버섯이 없습니다');
+        return mushroom;
+      }
+      onChangeHeight(-50);
+      return mushroom - ONE_MUSHROOM;
+    });
+  }, [onChangeHeight]);
+
+  return (
+    <>
+      <Button isRound onClick={onDrkinkBeverage}>
+        음료를 마시다
+      </Button>
+      <Button isRound onClick={onEatCake}>
+        케이크를 먹다
+      </Button>
+      <Button isRound onClick={onFan}>
+        부채질하다
+      </Button>
+      <Button isRound onClick={onEatMushroom}>
+        버섯을 먹다
+      </Button>
+      <Button isRound onClick={onGoOutside}>
+        문을 통과하다
+      </Button>
+    </>
+  );
+};
+
+export default AliceController;
--- src/components/alice-in-wonderland/index.tsx
+++ src/components/alice-in-wonderland/index.tsx
@@ -1,12 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../common/button';
-
-const ONE_BEVERAGE = 20;
-const ONE_CAKE = 50;
-const ONE_MUSHROOM = 10;
-
-type TLocation = '문 밖' | '문 안';
+import AliceController from './alice-controller';
 
 const Wrapper = styled.div``;
 
@@ -21,15 +16,13 @@ const FlexRow = styled.div`
   margin: 10px;
 `;
 
+type TLocation = '문 밖' | '문 안';
+
 const AliceInWonderland: React.FC = () => {
   const [print, setPrint] = useState<number | TLocation | null>(null);
 
   const [height, setHeight] = useState<number>(130);
   const [location, setLocation] = useState<TLocation>('문 밖');
-
-  const [beverage, setBeverage] = useState<number>(200); // ml
-  const [cake, setCake] = useState<number>(1000); // g
-  const [mushroom, setMushromm] = useState<number>(100); // g
 
   const onPrintHeight = useCallback(() => {
     setPrint(height);
@@ -48,50 +41,16 @@ const AliceInWonderland: React.FC = () => {
     });
   }, []);
 
-  const onDrkinkBeverage = useCallback(() => {
-    setBeverage((beverage) => {
-      if (beverage < ONE_BEVERAGE) {
-        alert('음료수가 없습니다');
-        return beverage;
-      }
-      return beverage - ONE_BEVERAGE;
-    });
-    onChangeHeight(-100);
-  }, [onChangeHeight]);
-
-  const onEatCake = useCallback(() => {
-    setCake((cake) => {
-      if (cake < ONE_CAKE) {
-        alert('케익이 없습니다');
-        return cake;
-      }
-      return cake - ONE_CAKE;
-    });
-    onChangeHeight(+80);
-  }, [onChangeHeight]);
-
-  // 부채질
-  const onFan = useCallback(() => {
-    onChangeHeight(+70);
-  }, [onChangeHeight]);
-
-  const onEatMushroom = useCallback(() => {
-    setMushromm((mushroom) => {
-      if (mushroom < ONE_MUSHROOM) {
-        alert('버섯이 없습니다');
-        return mushroom;
-      }
-      onChangeHeight(-50);
-      return mushroom - ONE_MUSHROOM;
-    });
-  }, [onChangeHeight]);
-
   const onGoOutside = useCallback(() => {
+    if (location === '문 안') {
+      return alert('이미 문 안입니다.');
+    }
     if (height < 40 || height > 80) {
       return alert('키가 40이상 80이하여야 문을 들어갈 수 있습니다.');
     }
-    setLocation('문 밖');
-  }, [height]);
+    alert('문 안으로 들어갑니다');
+    setLocation('문 안');
+  }, [height, location]);
 
   return (
     <Wrapper>
@@ -101,21 +60,7 @@ const AliceInWonderland: React.FC = () => {
         <Button onClick={onPrintLocation}>위치</Button>
       </FlexRow>
       <FlexRow>
-        <Button isRound onClick={onDrkinkBeverage}>
-          음료를 마시다
-        </Button>
-        <Button isRound onClick={onEatCake}>
-          케이크를 먹다
-        </Button>
-        <Button isRound onClick={onFan}>
-          부채질하다
-        </Button>
-        <Button isRound onClick={onEatMushroom}>
-          버섯을 먹다
-        </Button>
-        <Button isRound onClick={onGoOutside}>
-          문을 통과하다
-        </Button>
+        <AliceController onChangeHeight={onChangeHeight} onGoOutside={onGoOutside} />
       </FlexRow>
     </Wrapper>
   );
